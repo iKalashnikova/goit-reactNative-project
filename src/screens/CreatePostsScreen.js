c94a46d1158d08148ea8a0f374863ee77ad63dcf
iKalashnikova <ikalashnikova@ukr.net>
2023-06-12 22:23:30 +0200

    trashInCreateScreen

--- src/screens/CreatePostsScreen.js
+++ src/screens/CreatePostsScreen.js
@@ -15,198 +15,231 @@ import {
   StyleSheet,
   Platform,
   Keyboard,
-  TextInput
+  TextInput,
 } from "react-native";
-
 
 SplashScreen.preventAutoHideAsync();
 
 export const CreatePostsScreen = ({ navigation }) => {
+  const [photo, setPhoto] = useState("");
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
 
-    const [photo, setPhoto] = useState("");
-    const [title, setTitle] = useState("");
-    const [location, setLocation] = useState("");
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf"),
+  });
 
-    const [fontsLoaded] = useFonts({
-        "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
-        "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
-        "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf"),
-      });
-    
-      
-      const handlePublish = () => {
-        console.log("Опубліковано!");
-      };
+  const handlePublish = () => {
+    console.log("Опубліковано!");
+  };
 
-      const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-          await SplashScreen.hideAsync();
-        }
-      }, [fontsLoaded]);
-    
-      if (!fontsLoaded) {
-        return null;
-      }
-    
-      const isFormValid = title !== "" && location !== "";
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
-      return (
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : null}
-          keyboardVerticalOffset={-300}
-          style={styles.container}
-          onLayout={onLayoutRootView}
-        >
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.container}>
-              {/* Header */}
-              <View style={styles.header}>
-              <View style={styles.headerContent}>
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const isFormValid = title !== "" && location !== "";
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      keyboardVerticalOffset={-300}
+      style={styles.container}
+      onLayout={onLayoutRootView}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          {/* Header */}
+          <View style={styles.header}>
+            <View style={styles.headerContent}>
               <TouchableOpacity
               // onPress={back}
               >
                 <Icon name="arrow-back-outline" size={24} color="#000" />
               </TouchableOpacity>
-                <Text style={styles.headerText}>Створити Публікацію</Text>
-              
-             
-              </View>
-              </View>
-    
-              <View style={styles.container}>
-      <Text>"Завантажити фото"</Text>
-
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={setTitle}
-        placeholder="Назва..."
-      />
-      <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.inputLocation}
-        value={location}
-        onChangeText={setLocation}
-        placeholder="Місцевість..."    
-      />
-       <Icon name="location-outline" size={24} color="#000" style={styles.icon} />
-       </View>
-      <TouchableOpacity style={[styles.button, !isFormValid && styles.buttonDisabled]}
-                  onPress={handlePublish}
-                  disabled={!isFormValid}>
-        <Text style={styles.buttonText}>Опублікувати</Text>
-      </TouchableOpacity>
-    </View>
-  
-              <View style={styles.mainContent}>
-                {/* Список користувачів */}
-                {/* Додатковий вміст */}
-              </View>
+              <Text style={styles.headerText}>Створити Публікацію</Text>
             </View>
-          </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        width: "100%",
-        alignItems: "center",
-        // justifyContent: "space-around",
-        paddingTop: 24,
-        borderRadius: 25,
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-        backgroundColor: "#fff",
-        fontFamily: "Roboto-Regular",
-      },
-      header: {
-        height: 60,
-        paddingHorizontal: 16,
-        justifyContent: "center",
-        borderBottomWidth: 1,
-        width: "100%",
-        borderBottomColor: "#ccc",
-      },
-      headerContent: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-      },
-      headerText: {
-        flex: 1,
-        fontSize: 17,
-        fontWeight: "bold",
-        textAlign: "center",
-        fontFamily: "Roboto-Medium",
-      },
-      label: {
-        fontSize: 16,
-        fontWeight: "bold",
-        marginBottom: 5,
-      },
-      inputContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-      },
-      input: {
-        width: "90%",
-        height: 40,
-        borderColor: "#ccc",
-        borderBottomWidth: 1,
-        borderTopWidth: 0,
-        borderLeftWidth: 0,
-        borderRightWidth: 0,
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        marginBottom: 20,
-        paddingLeft: 10,
-      },
-      inputLocation: {
-        position: "relative",
-        width: "90%",
-        height: 40,
-        borderColor: "#ccc",
-        borderBottomWidth: 1,
-        borderTopWidth: 0,
-        borderLeftWidth: 0,
-        borderRightWidth: 0,
-        borderRadius: 5,
-        paddingHorizontal: 20,
-        marginBottom: 20,
-        paddingLeft: 40,
-      },
-      icon: {
-        // marginRight: 15,
-        position: 'absolute',
-        bottom: 30,
-        left: 10,
-        right: 5,
-        color: "#ccc",
-        
-      },
-      button: {
-       
-        alignItems: "center",
-        padding: 16,
-        paddingHorizontal: 32,
-        marginBottom: 16,
-        marginTop: 27,
-    
-        width: "90%",
-        // alignSelf: "stretch",
-        backgroundColor: "#FF6C00",
-        borderRadius: 100,
-      },
-      buttonText: {
-        color: "#fff",
-        fontWeight: "normal",
-        fontSize: 16,
-      },
-      buttonDisabled: {
-        backgroundColor: '#ccc',
-        opacity: 0.5,
-      }
-    });
-    
+          </View>
+
+          <View style={styles.container}>
+            <Text>"Завантажити фото"</Text>
+
+            <TextInput
+              style={styles.input}
+              value={title}
+              onChangeText={setTitle}
+              placeholder="Назва..."
+            />
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputLocation}
+                value={location}
+                onChangeText={setLocation}
+                placeholder="Місцевість..."
+              />
+              <Icon
+                name="location-outline"
+                size={24}
+                color="#000"
+                style={styles.icon}
+              />
+            </View>
+            <TouchableOpacity
+              style={[styles.button, !isFormValid && styles.buttonDisabled]}
+              onPress={handlePublish}
+              disabled={!isFormValid}
+            >
+              <Text style={styles.buttonText}>Опублікувати</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.mainContent}>
+            {/* Список користувачів */}
+            {/* Додатковий вміст */}
+          </View>
+          <View style={styles.footer}>
+            <View style={styles.trashIconWraper}>
+          <Icon
+                name="trash-outline"
+                size={24}
+                color="#000"
+                style={styles.trashIcon}
+              />
+              </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    // justifyContent: "space-around",
+    paddingTop: 24,
+    borderRadius: 25,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    backgroundColor: "#fff",
+    fontFamily: "Roboto-Regular",
+  },
+  header: {
+    height: 60,
+    paddingHorizontal: 16,
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    width: "100%",
+    borderBottomColor: "#ccc",
+  },
+  headerContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerText: {
+    flex: 1,
+    fontSize: 17,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Roboto-Medium",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    width: "90%",
+    height: 40,
+    borderColor: "#ccc",
+    borderBottomWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+  inputLocation: {
+    position: "relative",
+    width: "90%",
+    height: 40,
+    borderColor: "#ccc",
+    borderBottomWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+    paddingLeft: 40,
+  },
+  icon: {
+    // marginRight: 15,
+    position: "absolute",
+    bottom: 30,
+    left: 10,
+    right: 5,
+    color: "#ccc",
+  },
+  button: {
+    alignItems: "center",
+    padding: 16,
+    paddingHorizontal: 32,
+    marginBottom: 16,
+    marginTop: 27,
+
+    width: "90%",
+    // alignSelf: "stretch",
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "normal",
+    fontSize: 16,
+  },
+  buttonDisabled: {
+    backgroundColor: "#ccc",
+    opacity: 0.5,
+  },
+  footer: {
+    // flex: 1,
+    flexDirection: "row",
+    height: 60,
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 30,
+    width: "100%",
+  },
+  trashIcon: {
+    alignItems: "center",
+    color: "#fff"
+  },
+  trashIconWraper:{
+    backgroundColor: '#ccc',
+    opacity: 0.5,
+    width: 70,
+    height: 40,
+    borderRadius: 50,
+    justifyContent: 'center',
+      alignItems: 'center',
+  }
+});
